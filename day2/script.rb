82197ec49e4e561fb2ff48e59222261ea1ef7b05
chrismo <chrismo@clabs.org>
2018-12-04 10:46:08 -0600

    Backfill Day 2 from scratch file.

---
+++ day2/script.rb
@@ -0,0 +1,294 @@
+inputs = %w(
+tqzvwnogbarflkpcbdewsmjhxi
+tqyvunogzarfckpcbdewsmzhci
+tqyvunojzarflkpcbdcwsmyhxi
+tqyvunogzarclkpcbdewmmjrxi
+hqyvunogzarflkpcbczwsmjhxi
+tqyvunogzarflppcudewsmjhei
+tqrvunogzarflkpcbpewsmjhdi
+aqyvunogzarflkpcbdewsjjsxi
+tqyvtnogzarflkkcbdewymjhxi
+tcyeunogzarfkkpcbdewsmjhxi
+tqyvunozzarfvkpcbdewsqjhxi
+tkyvuwjgzarflkpcbdewsmjhxi
+tqevunogzarflkpnbdkwsmjhxi
+tqyvunogqarflkpcppewsmjhxi
+tqyvunsgzarflkpcbrewsmjhxk
+tqyvunogzarffkpdbnewsmjhxi
+tqyvunogvarflkpjbdewsojhxi
+tqyvgnogzarflkpfbdswsmjhxi
+tqyvunogzarfxkpcbtersmjhxi
+tqyvukhgzarflupcbdewsmjhxi
+tqyvdnoozyrflkpcbdewsmjhxi
+tqyvunogzorflkpcbdewsvjhxd
+tqyvunzqzarflkpcbdewxmjhxi
+tqykunogzarulkpcbdhwsmjhxi
+tqycdnogzarflkpcbdewsmjhxz
+eqyvunogzarflkpcbdhwqmjhxi
+cqyvunogzaralkpcbdewsmjvxi
+vqyvunogzarflklcbeewsmjhxi
+tqyvunogzarffkpcqdewlmjhxi
+eqyvunogzarflkpcbdejsmahxi
+tqyvunogjarflkocsdewsmjhxi
+tqzvunogzanflkpcbdewsmjhxg
+tqycunogzarflkpabdewsmkhxi
+tqyvunogzarnlkpcbdewimjaxi
+tqyvunogzarfzkpcbdepsyjhxi
+gqykunogzarelkpcbdewsmjhxi
+tqyvuwogzarflkpcbdrwsmjmxi
+tqdqunogzarflkpcbdewsvjhxi
+tqmvunbgzarflkpcbdewsvjhxi
+tqyvunogzarflkpcbheesmjhdi
+tqxvunogzarfxkpcbdewsmhhxi
+tqyvunogzarflkpabdjosmjhxi
+tqyvugogztrflkpybdewsmjhxi
+tqyvundgzarflkxcbdewsmjhmi
+tqyvunogzurfgkpcbdeksmjhxi
+tqyvunokzarfckpcbdewsojhxi
+tqyvufobzarflkpcldewsmjhxi
+tqyvunogznrflkncbdeusmjhxi
+tqyvuncgzarfxkpcmdewsmjhxi
+oqyvunogzarflkpybdewbmjhxi
+tqyvjnogqarfmkpcbdewsmjhxi
+tqyvunogzacflkpcidewsmjhwi
+tqyvunogzarflkpcbqlwxmjhxi
+tqyvunogzarflkpnbhewsvjhxi
+vqyiunogzarflkpcbdewsmjhqi
+tbyvuncgzarfljpcbdewsmjhxi
+tqylunogzarflkpcldexsmjhxi
+tqyvulogzarflktcbdewsxjhxi
+tqyvmnlgzarflkpcbxewsmjhxi
+tqyvunogzartlkpcbdewsmihxp
+nqyvunogzarflkpcbdewsmnwxi
+tqyvunogzarflkpczdewsmjcxj
+tzyvunogzariwkpcbdewsmjhxi
+tqyiufogzarflipcbdewsmjhxi
+oqyvunogzasflkpcbdewsmjhxv
+tqyvunoqmarflkpcwdewsmjhxi
+tqrvunogzarflkpqbdewnmjhxi
+tqyvunogzarmlkocbdewsmjhri
+tqyvunogzakflkpcbveasmjhxi
+tqyvunorearflkpcbdewsmfhxi
+tqynrnogzarflkpcbdewsmjhxp
+tiyvueogzaeflkpcbdewsmjhxi
+tqyrunogzarflkpccdewbmjhxi
+tqtvunogzarflkpcbdewbnjhxi
+tqyvfnogzarflhpcbdewsmjqxi
+tqyjunoazarflkpcbdewssjhxi
+tqyvunxizarflkpcbdewsmjnxi
+tqyvunogzarfhkpobdewsmjhai
+tqyaunogzanflkpcbxewsmjhxi
+tqyvunogzarflkpsbuewsmjmxi
+tqyvunogzarzlkwybdewsmjhxi
+tqyvunogzarflkpibdawsmhhxi
+tqyvunogzarflkycbdewamjwxi
+tqyvunourarflkpcbdewsujhxi
+tqyvnnogzirflkpcbdewsdjhxi
+tayvunogzauflkpcqdewsmjhxi
+tqyvunobzfrflkscbdewsmjhxi
+tqygvnogzarflkpcbdewsmjnxi
+oqyvunogzarflkpcbdewsmjsgi
+tqyvunokzarflkpcbdewshjhii
+tqyvunobzarflkvcbdewskjhxi
+aqyvunogvarflkpcbdlwsmjhxi
+tqyvunogzmrrlbpcbdewsmjhxi
+tqyvunggzaqolkpcbdewsmjhxi
+tqyvunogqarflkpybdewsmjaxi
+tqyvunogzxrflkpcxsewsmjhxi
+zqyvunogzarflppcbdewsmjhni
+tqvvunogzakslkpcbdewsmjhxi
+tqyrunogzarzlkpcbdewsmjtxi
+tqyvhnogzarfxkpcbdewqmjhxi
+tqyvunogzarflkecbdewgdjhxi
+tqyvuwogzerfhkpcbdewsmjhxi
+tqmvunogzarflkpcbddwsmdhxi
+tqyvunogzarflcqcbdewsmihxi
+tqyvunogzarvlkpcbdewsmjmxd
+tqyvknogzarfllncbdewsmjhxi
+tqyvunogzarflbpcbdrwsajhxi
+tqyvunogzarfukpcbddwsmjhii
+tqyvuvojzahflkpcbdewsmjhxi
+tqyvunogzarfchpcbdeqsmjhxi
+wqivueogzarflkpcbdewsmjhxi
+tqyvunogzwrflkpcbdewstjhxz
+tqyvunogzarfloscbdewsmjhxf
+tqfbuiogzarflkpcbdewsmjhxi
+tqyvfuogzarflkpcxdewsmjhxi
+tqyvunogzarflkpcpdewsmgqxi
+tqyvunogzdrflkpcbdewsmqhci
+tqyvunogzartlkpcbdewsmjpxj
+tqyfunogzarfwkpcbdewsmwhxi
+tqyvuntgzarflkjcmdewsmjhxi
+tqyvunqfzarflkpckdewsmjhxi
+nqyvunogznrflkpcbiewsmjhxi
+tqymunobzarflkccbdewsmjhxi
+tqyvunogzaoflkprbdewzmjhxi
+tqyvunogzaiflkpcvdewbmjhxi
+tqwvunogzarfzkpcbdewsmjhxx
+txhvunogzarflkpcbdewsijhxi
+tqyeunogkarflkicbdewsmjhxi
+tqylunogzarylkpcbdewsmkhxi
+tqyvriogzarflkpcbdewsmohxi
+tqyvunogzsrflkpcbdeasijhxi
+tqyvunogzarflkpcbfewcmjhxh
+tqyvunoozvrflkpcbdewimjhxi
+tqyvunogqayflkpcidewsmjhxi
+tqyounogzarflkpccdewsmjhxg
+tqgvunogsarflkpcbdewqmjhxi
+tqevunogzarflkpcbmewsmjhpi
+tqivunogzarflkgcbdewqmjhxi
+tqyuunogzlrflkgcbdewsmjhxi
+xqyvbnogznrflkpcbdewsmjhxi
+tqyvunogzarfjkpebdewsmnhxi
+tqyvvnogzarfskpcxdewsmjhxi
+thovunogzarflkpcbdewsmjhvi
+tqyvunugzarflkpcpdewsmjrxi
+tcyvvnogzarflkdcbdewsmjhxi
+tqdfunogzarflkpbbdewsmjhxi
+tqyvunogzarflkpcbdnwsejzxi
+tqyvunivkarflkpcbdewsmjhxi
+tqyvunogzawflopcbdewsmjhmi
+tqyvunogzarflkpcbdkwsdjqxi
+tqyvunodzarflkpcbdewlmjhei
+oqyvunoozarflkpcbdemsmjhxi
+tiyvunogzarjlkpcbdewfmjhxi
+tqrvunogearflkpcbdewsojhxi
+tqyvunkgzarflkpcbdcwtmjhxi
+tqmvunogzarflkpcbdpwsmjtxi
+tqyvunogzarflkpcydeptmjhxi
+tqyvunkglarflkpcbdfwsmjhxi
+tqyaunogzarflkpcbzeesmjhxi
+tqyvunogzarrlkpcbdkwsmjhui
+tpyvunogzarflkqcbdewsmjhxd
+tvyvunogzarfkkpcbdewsajhxi
+gqynunogzarflepcbdewsmjhxi
+zqvvunogzarflkpcbdexsmjhxi
+tqyyunogzawflkpcbdewsmjhxw
+tfyvunogzarflkpcbdewomjrxi
+tqyeunogzvrflrpcbdewsmjhxi
+nqyvunogzarftkpabdewsmjhxi
+tzyvunogzariwkpcbdewmmjhxi
+tiyvunogzarflkpcbbewsmjhxa
+tqydujoyzarflkpcbdewsmjhxi
+tqyvunpgzarflkpcbdeysmjhwi
+tqyvunogvarllkpcbdewsmshxi
+tqyvunogzbrvlkpcbdewsmjhxp
+tcyvueogzarflkacbdewsmjhxi
+tqyvunogzrhflkpcbdetsmjhxi
+tqavunogzrdflkpcbdewsmjhxi
+tqyvunogzjrflkpcbdewstjhki
+tqyqunolzarflkpcbdewbmjhxi
+tqyvunogzarflkqczdgwsmjhxi
+tqyvunogzqrfrkpcbrewsmjhxi
+tqyvcnogzhrflkacbdewsmjhxi
+tqyvunogzarflkpcbdewsmdhtk
+tqyvunoggarftkpcbjewsmjhxi
+tvyvunogzarfkkpcpdewsmjhxi
+tqyvunogzawflkpcndedsmjhxi
+tqyvunogzrrflkpcbdemsmmhxi
+tqyvunogzarclkpcbrpwsmjhxi
+tryvunogztrflkpcbbewsmjhxi
+cqyvundgzarflkpcbdewgmjhxi
+tqyvunogzarflktcbkewsmjqxi
+tqyvjuogzarflkpcadewsmjhxi
+tqyvunogzyrflkpcbbxwsmjhxi
+ttyvunogzarflkpcbdewsnmhxi
+tqyvunogzarflkpcbdeqsmlhki
+fqyvugogzarflapcbdewsmjhxi
+tqyvunogzartlkppbdewszjhxi
+tqyvunfgztrflzpcbdewsmjhxi
+tqyvunogmazflkpcbdewsmjhki
+tqyvunzdzarflkpcbdewsmjhvi
+tqyvunogzarflkpqbzewsujhxi
+tqyvunogzarzlkpcbeewymjhxi
+tqyounogzarflkpcbdewsnwhxi
+tqysunogsaiflkpcbdewsmjhxi
+tqdvunogdarflkpcboewsmjhxi
+teyvunogzarflkscbdfwsmjhxi
+tqyvunoazarflkpcbdvwsmjhpi
+tqyvunooearflkpcbdewrmjhxi
+tqyvunoszarflnrcbdewsmjhxi
+tqyvunogzalflkpcblewsjjhxi
+uqlvunkgzarflkpcbdewsmjhxi
+tqyvunobzarflkpcidewsmjhvi
+tnyvunogzarflkpcbdnwamjhxi
+tqyoudogzarflkpcbdgwsmjhxi
+tqyvunoggarflkpcbmewsmwhxi
+tqykunogzazflkpcbddwsmjhxi
+tfyvunogzarflkpcbdewsmjhgo
+tqyvunogztrflkpcbdewoojhxi
+tqyvunogzarflkpcbdewbmjoni
+tmyvunogzalflkpabdewsmjhxi
+tqyvunogzarflkpbbvewqmjhxi
+tqyvunofzarflkpcwdexsmjhxi
+tayvunogzasflkpcbdewsmhhxi
+tqyvlnogzarflkpcbdewsmjwxd
+tvyvunogzarflkpcbdewhmjrxi
+tqyvunogzarplkpcsdewsmihxi
+tqyvunogzarplklcbdewsmjtxi
+rqbvunogzarhlkpcbdewsmjhxi
+tqyvuxogzarftkpcbdewsmthxi
+tqtvunogzarfikpczdewsmjhxi
+tqyvunwgzarflkpcbdewsmjxxk
+tqygunogzarzlkpnbdewsmjhxi
+tqyvunogzarjlkpcbdbwswjhxi
+tqyvunogzalfnkpcbdewsmjhxf
+tqyucuogzarflkpcbdewsmjhxi
+tzyvunogzvrflkpcbdewsmjaxi
+tjyvunlgzarflkpcbdewgmjhxi
+tqyvcnogzarklkpcbdewsmjhfi
+tqyvunogzaaflkpsbaewsmjhxi
+tsyvunogzarflkpctdewsmbhxi
+tqyeunbgzarflkpcbdewrmjhxi
+tqyvunogzcrflqpcbdeismjhxi
+eqylunogzarflkpcbdewsmjhxy
+tqyvundgzarflkpcbdewmmnhxi
+tzyvunogzarflkpcndewsmjhxb
+tkyvunogzxrflkpcbdewfmjhxi
+tqyvunogzarflkxcbdzwsmjfxi
+kqavunogzarflkycbdewsmjhxi
+tqyvunogzarjlkpcbdxwkmjhxi
+tqyvinogzarfqkpcbdewsmjpxi
+)
+
+inputs.map! { |s| s.each_char.to_a }
+
+# Part 1
+
+two_counts = three_counts = 0
+inputs.each do |input|
+  counts = input.group_by(&:itself).map { |_, a| a.length }.uniq
+  two_counts += 1 if counts.include?(2)
+  three_counts += 1 if counts.include?(3)
+end
+p [two_counts, three_counts]
+p two_counts * three_counts
+
+# Part 2
+
+inputs.each do |outer|
+  inputs.each do |inner|
+    diffs = 0
+    0.upto(outer.length - 1) { |i| diffs += 1 if outer[i] != inner[i] }
+    if diffs == 1
+      puts [outer.join, inner.join]
+      return
+    end
+  end
+end
+
+# tzyvunogzariwkpcbdew s mjhxi
+# tzyvunogzariwkpcbdew m mjhxi
+#
+# tzyvunogzariwkpcbdewmjhxi
+
+
+# Nice solution posted on reddit
+# a = $<.map(&:chars)
+# a.map { |x|
+#   a.map { |y|
+#     d = x.zip(y).count { |x, y| x != y }
+#     (p x.join, y.join
+#     1 / 0) if d == 1
+#   }
+# }
